cf641b0adffa8b033c4460f1fe9ea432eaf71996
Grzesiek <grzegorz.kwasniewski@itcraft.pl>
2024-11-02 18:49:59 +0100

    Update project structure

---
+++ MySpendingApp/Presentation/Login/SignInView.swift
@@ -0,0 +1,19 @@
+//
+//  SignInView.swift
+//  MySpendingApp
+//
+//  Created by Grzesiek on 29/10/2024.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    SignInView()
+}
